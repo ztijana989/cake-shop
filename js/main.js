6a7fab90a65baf66b671e46b7d065a0b28232dcc
Tijana Zaric <ztijana989@gmail.com>
2019-03-10 11:08:11 +0100

    banner in pogress

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 $(document).ready(function () {
+	// Hamburger 
 	$('.js-burger').on('click', function() {
 		$(this).toggleClass('open');
-		$('.js-nav').toggleClass('open')
+		$('.js-nav').toggleClass('open');
+	});
+
+	console.log('aaa');
+
+	// Slick slider
+	$('.js-hero-slider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		speed: 1000,
+		arrows: true,
+		autoplay: true,
+		dots: true,
 	});
 });
