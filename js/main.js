e3febe10357a5e52511544cc7c5b9aae37f03781
Tijana Zaric <ztijana989@gmail.com>
2019-03-11 12:19:46 +0100

    section-choose completed

--- js/main.js
+++ js/main.js
@@ -5,16 +5,57 @@ $(document).ready(function () {
 		$('.js-nav').toggleClass('open');
 	});
 
-	console.log('aaa');
-
 	// Slick slider
+	// Hero slider
 	$('.js-hero-slider').slick({
 		infinite: true,
 		slidesToShow: 1,
 		slidesToScroll: 1,
-		speed: 1000,
+		speed: 1500,
 		arrows: true,
 		autoplay: true,
+		autoplaySpeed: 3000,
 		dots: true,
+		responsive: [
+	    {
+	      breakpoint: 992,
+	      settings: {
+	        arrows: false,
+	      }
+	    }
+	  ]
+	});
+
+	// Choose slider
+	$('.js-choose-slider').slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		speed: 1500,
+		arrows: true,
+		responsive: [
+	    {
+	      breakpoint: 1200,
+	      settings: {
+	        slidesToShow: 3,
+	      }
+	    },
+	    {
+	      breakpoint: 992,
+	      settings: {
+	        arrows: false,
+	        dots: true,
+	        slidesToShow: 2,
+	      }
+	    },
+	    {
+	      breakpoint: 640,
+	      settings: {
+	      	arrows: false,
+	        dots: true,
+	        slidesToShow: 1,
+	      }
+	    }
+	  ]
 	});
 });
